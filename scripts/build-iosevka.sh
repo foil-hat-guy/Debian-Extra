81a6278d17f9cedf5ad582edcd35c9f1c28500f9
foil-hat-guy <dev@foilhatguy.casa>
2023-08-07 02:41:08 +0300

    Create build-iosevka.sh

---
+++ scripts/build-iosevka.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mkdir -p ./$basename$fontdir
+cp ./source/$basename-Bold.ttf ./$basename$fontdir-Bold.ttf
+cp ./source/$basename-BoldItalic.ttf ./$basename$fontdir-BoldItalic.ttf
+cp ./source/$basename-Italic.ttf ./$basename$fontdir-Italic.ttf
+cp ./source/$basename-Regular.ttf ./$basename$fontdir-Regular.ttf
+
+mkdir -p ./$basename/DEBIAN
+touch ./$basename/DEBIAN/control
+
+cat <<EOT >> ./$basename/DEBIAN/control
+Package: $pkgname
+Maintainer: Foil-hat-guy \<dev@foilhatguy.casa\> 
+Priority: optional 
+Essentional: no 
+Architecture: all 
+Section: fonts 
+Homepage: https://www.nerdfonts.com/ 
+Version: $(echo $pkgversion | cut -d v -f 2) 
+Installed-Size: $(du -sk ./$basename | cut -f 1) 
+Description: Iosevka nerd font.
+\ This package contains regular, bold, italic and bold italic fonts.
+$1
+EOT
